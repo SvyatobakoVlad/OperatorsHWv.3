577896f1ea62fa6102f11390bc2d14cb1187be43
SvyatobatkoVlad <svyatobatko199@gmail.com>
2020-02-11 16:24:18 +0200

    Home work 2

---
+++ src/com/itbulls/learnit/HW2svyatobatko/DemoHW2.java
@@ -0,0 +1,13 @@
+package com.itbulls.learnit.HW2svyatobatko;
+
+public class DemoHW2 {
+	public static void main(String[] args) {
+		HW2.location();
+		HW2.words();
+		HW2.getPiramide ();
+		HW2.numbersMus();
+	}
+
+}
+
+
