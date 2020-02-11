46c7c2629bb9d67db0775d8b6ebd442dcfcfbf5c
SvyatobatkoVlad <svyatobatko199@gmail.com>
2020-02-11 13:00:23 +0200

    kek Local repos kek

---
+++ src/com/itbulls/learnit/svyatobatko/Demo.java
@@ -0,0 +1,13 @@
+package com.itbulls.learnit.svyatobatko;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		FirstHW.one_Sum();
+		FirstHW.two_Area();
+		FirstHW.three_radius();
+		
+	}
+
+	
+}
