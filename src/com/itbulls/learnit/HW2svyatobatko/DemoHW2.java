ebf2eb7d26b57c5cee9cbf9320d5af49d8876a7d
SvyatobatkoVlad <svyatobatko199@gmail.com>
2020-02-12 16:19:08 +0200

    Add Home work 2

--- src/com/itbulls/learnit/HW2svyatobatko/DemoHW2.java
+++ src/com/itbulls/learnit/HW2svyatobatko/DemoHW2.java
@@ -4,10 +4,9 @@ public class DemoHW2 {
 	public static void main(String[] args) {
 		HW2.location();
 		HW2.words();
-		HW2.getPiramide ();
+		HW2.getPiramide();
 		HW2.numbersMus();
+		
 	}
 
 }
-
-
--- src/com/itbulls/learnit/HW2svyatobatko/HW2.java
+++ src/com/itbulls/learnit/HW2svyatobatko/HW2.java
@@ -1,4 +1,5 @@
 package com.itbulls.learnit.HW2svyatobatko;
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -6,80 +7,86 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class HW2 {
-	
-	
-	
-	
-	public static void location() {
-	int a;
-	Scanner sc = new Scanner (System.in);
-	a = sc.nextInt();
-	String text = String.join("\n", String.valueOf(a).split(""));
-		System.out.println(text); 
-		}
-	
-	
-	
-	
-	public static void words() {
-		String[] array = {"asd", "asdf", "a", "Z", "zasd"};
-		Arrays.sort(array, Comparator.reverseOrder());
-		Arrays.sort(array, 
-				(str1, str2) -> str1.compareToIgnoreCase(str2));
-		System.out.println(Arrays.toString(array));
-	
-	
-	
-//		Arrays.sort(strings, Comparator.reverseOrder());
-//		assertArrayEquals(new String[]{ "with", "learning", "java", "baeldung" }, strings);
-//	System.out.println(Arrays.toString(array));
 
+	public static void location() {
+		int a;
+		System.out.println("¬ведите число");
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		String text = String.join("\n", String.valueOf(a).split(""));
+		System.out.println(text);
+	}
+
+	public static void words() {
+
+		System.out.println("Enter any 7 words:");
+		Scanner sc = new Scanner(System.in);
+		String inWords = sc.nextLine();
+		String[] arr = inWords.split(" ");
+		Arrays.sort(arr, (str1, str2) -> str2.length() - str1.length());
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static void getPiramide() {
+		System.out.println("¬ведите высоту пирамиды");
+		int n = 7;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j < n - (1 + i)) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void numbersMus() {
+		System.out.println("—осздаем массив");
+		Random rand = new Random();
+		int mas[] = new int[10];
+		int mas2[] = new int[10];
+		for (int i = 0; i < 10; i++) {
+			mas[i] = rand.nextInt(20);
+			System.out.print(mas[i] + " ");
+		}
+		for (int x = 0; x < mas.length; x++) {
+			mas[x] = mas[x] * 2;
+		}
+
+		for (int i = 0; i < mas.length; i++) {
+			System.out.print(mas[i] + " ");
+		}
+
+	}
+	
+	
+
+	
+			
+			
 	}
 	
 	
 	
 	
-
-
-
-
-    public static void getPiramide() {
-       
-    	int n = 7;
-    	
-    	for (int i = 0; i < n; i++) {
-    		for (int j = 0; j < n; j++) {
-    			if (j < n-(1+i)) {
-    				System.out.print(" ");
-    			}
-    			else {
-    				System.out.print("*");
-    			}
-    	}
-    		System.out.println();
-    	}
-    	
-}
-
 	
-    
-    
-    public static void numbersMus() {
-    Random rand = new Random();
-    int mas[] = new int [10];
-    int mas2[] = new int [20]; 
-    	for(int i = 0; i < 10; i++) 
-    	{
-    		
-    		mas[i] = rand.nextInt(20);
-//    		int a=5;
-//			mas2[a] = mas[i]*2;
-    		
-    		System.out.print(mas[i] + " ");
-//    		System.out.print(mas[i] + " " + mas2[a]);
-        	
-    	
-    }
 	
-}
-}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
